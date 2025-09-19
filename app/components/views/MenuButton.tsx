f46f498714b4b236679b4602820a00696a5b4383
19Vako <152034949+19Vako@users.noreply.github.com>
2025-09-19 17:09:33 +0300

    add MenuButton.tsx

---
+++ app/components/views/MenuButton.tsx
@@ -0,0 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import normalize from "react-native-normalize";
+import { useOpenMenu } from "../viewsModels/MenuButtonViewModel";
+
+
+export function MenuButton() {
+  const openMenu = useOpenMenu()
+  return (
+    <TouchableOpacity
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#424d57",
+        height: normalize(30),
+        width: normalize(30),
+        borderRadius: normalize(10),
+        marginLeft: normalize(15),
+      }}
+      onPress={openMenu}
+    >
+      <Ionicons name="menu" size={normalize(20)} color="#637483" />
+    </TouchableOpacity>
+  );
+}
+
