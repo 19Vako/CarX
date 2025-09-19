a94d12953eb17724fe89278b943040265d33923f
19Vako <152034949+19Vako@users.noreply.github.com>
2025-09-19 17:11:51 +0300

    add icons to Drawer Labels and update styles of <Drawer.Navigator/>

--- app/navigation/DrawerNavigation.tsx
+++ app/navigation/DrawerNavigation.tsx
@@ -1,35 +1,73 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { MenuButton } from "../components/views/MenuButton";
 import Account from "./views/Account";
 import Activity from "./views/Activity";
 import Cars from "./views/Cars";
 import HomeScreen from "./views/HomeScreen";
 
-const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-
 export default function DrawerNavigation() {
-
   return (
-   <Drawer.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "transparent",
-        elevation: 0, // для Android
-        shadowOpacity: 0, // для iOS
-      },
-      headerTransparent: true,
-      headerTitle: "",
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "transparent",
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        drawerStyle: {
+          backgroundColor: "#21262c",
+        },
+        drawerActiveTintColor: "#fff",
+        drawerInactiveTintColor: "#ccc",
+        drawerLabelStyle: {
+          fontSize: 16,
+          fontWeight: "bold",
+        },
+        headerLeft: () => <MenuButton />,
+        headerTransparent: true,
+        headerTitle: "",
+      }}
+    >
+     <Drawer.Screen
+    name="HomeScreen"
+    component={HomeScreen}
+    options={{
+      drawerIcon: ({ color, size }) => (
+        <Ionicons name="home-outline" size={size} color={color} />
+      ),
     }}
-   >
-    
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="Account" component={Account} />
-      <Stack.Screen name="Activity" component={Activity} />
-      <Stack.Screen name="Cars" component={Cars} />
-      
-   </Drawer.Navigator>
+  />
+  <Drawer.Screen
+    name="Account"
+    component={Account}
+    options={{
+      drawerIcon: ({ color, size }) => (
+        <Ionicons name="person-outline" size={size} color={color} />
+      ),
+    }}
+  />
+  <Drawer.Screen
+    name="Activity"
+    component={Activity}
+    options={{
+      drawerIcon: ({ color, size }) => (
+        <Ionicons name="bar-chart-outline" size={size} color={color} />
+      ),
+    }}
+  />
+  <Drawer.Screen
+    name="Cars"
+    component={Cars}
+    options={{
+      drawerIcon: ({ color, size }) => (
+        <Ionicons name="car-outline" size={size} color={color} />
+      ),
+    }}
+  />
+    </Drawer.Navigator>
   );
 }
