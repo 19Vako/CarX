c6130d2a6e303b92074a20c0d848c420da074ef0
19Vako <152034949+19Vako@users.noreply.github.com>
2025-10-23 00:10:20 +0300

    add: dev dependency

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,44 @@
 import * as Font from "expo-font";
-import React from "react";
 import { Dimensions, View } from "react-native";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import store, { persistor } from "../app/store/store";
 import Navigation from "./navigation/Navigation";
+import store, { persistor } from "./store/store";
+
+
+import { usePushNotifications } from "./notifications/useNotification";
 
 const { height, width } = Dimensions.get("window");
 
+
+
 export default function App() {
+
+
+  const {expoPushToken, notification} = usePushNotifications()
+ 
+
   const [fontsLoaded] = Font.useFonts({
     "SpaceMono-Regular": require("@/assets/fonts/SpaceMono-Regular.ttf"),
     "Roboto-Medium": require("@/assets/fonts/Roboto-Medium.ttf"),
   });
-
   if (!fontsLoaded) {
     return null;
   }
 
+
+
   return (
+    
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <View style={{ position: "absolute", height, width }}>
           <Navigation />
         </View>
       </PersistGate>
-    </Provider>
+    </Provider> 
+  
+
   );
 }
