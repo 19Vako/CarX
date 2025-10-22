72d44bb68a2b23a490b2b10cee9319d47fb6e34e
19Vako <152034949+19Vako@users.noreply.github.com>
2025-10-23 00:06:00 +0300

    add hooks/deviceType.ts

---
+++ app/hooks/deviceType.ts
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native"
+
+export function deviceType() {
+  const { width } = Dimensions.get("window")
+  const isTablet = width >= 600
+
+  return { isTablet }
+}
