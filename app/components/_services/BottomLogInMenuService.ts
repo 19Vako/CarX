27e76d749583f655c0dad2fb67b4f5ee44c89cee
19Vako <152034949+19Vako@users.noreply.github.com>
2025-11-20 23:59:34 +0200

    update:BottomLogInMenuService

--- app/components/_services/BottomLogInMenuService.ts
+++ app/components/_services/BottomLogInMenuService.ts
@@ -5,15 +5,11 @@ import { useEffect } from "react";
 WebBrowser.maybeCompleteAuthSession();
 
 export function BottomLogInMenuService() {
-  const redirectUri = "http://localhost:8081";
-  console.log("URI перенаправления для Expo Go:", redirectUri);
   const [request, response, promptAsync] = Google.useAuthRequest({
     iosClientId:
       "561248963797-ng585ieq3ael47icplmhdll0gbnl52p1.apps.googleusercontent.com",
     androidClientId:
       "561248963797-pcdcc4oskmk8i6ca0176v2heceliv55p.apps.googleusercontent.com",
-    webClientId:
-      "561248963797-118ubesdg69am2d7374bv09blku4kqhs.apps.googleusercontent.com",
   });
 
   useEffect(() => {
