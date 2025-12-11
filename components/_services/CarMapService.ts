7a00d773de3b18fedb633ab7993cfee354250621
19Vako <152034949+19Vako@users.noreply.github.com>
2025-12-11 21:25:42 +0200

    relocate: components

---
+++ components/_services/CarMapService.ts
@@ -0,0 +1,15 @@
+import * as Location from "expo-location";
+import { LocationModel } from "../_models/CarMapModel";
+
+export const requestLocation = async (): Promise<LocationModel | null> => {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    return null;
+  }
+
+  const pos = await Location.getCurrentPositionAsync({});
+  return {
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude,
+  };
+};
