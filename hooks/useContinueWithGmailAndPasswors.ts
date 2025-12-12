280c0f49674fa3b0ff352a6be1d1759a696d1d27
19Vako <152034949+19Vako@users.noreply.github.com>
2025-12-12 10:36:01 +0200

    add: hooks/useContinueWithGmailAndPasswors.ts

---
+++ hooks/useContinueWithGmailAndPasswors.ts
@@ -0,0 +1,28 @@
+import { auth } from "@/firebaseConfig";
+import { useNavigation } from "@react-navigation/native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export function useContinueWithGmailAndPasswors() {
+  const navigation = useNavigation();
+  
+  const signIn = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCredential.user;
+      navigation.navigate("Account" as never);
+      console.log("Пользователь создан:", user.uid);
+      return user;
+    } catch (error: any) {
+      console.error("Ошибка регистрации:", error.message);
+      throw error;
+    }
+  };
+
+  return {
+    signIn,
+  };
+}
