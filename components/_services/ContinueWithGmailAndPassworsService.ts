5f22c5c0e3f8db4b8dfb8ae4c238ecf3d6cbc398
19Vako <152034949+19Vako@users.noreply.github.com>
2025-12-12 10:36:35 +0200

    update: ContinueWithGmailAndPassworsService.ts

--- components/_services/ContinueWithGmailAndPassworsService.ts
+++ components/_services/ContinueWithGmailAndPassworsService.ts
@@ -1,25 +1,7 @@
-import { auth } from "@/firebaseConfig";
-import { useNavigation } from "@react-navigation/native";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { useContinueWithGmailAndPasswors } from "@/hooks/useContinueWithGmailAndPasswors";
 
-export function useContinueWithGmailAndPassworsService() {
-  const navigation = useNavigation();
-  const signIn = async (email: string, password: string) => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password,
-      );
-      const user = userCredential.user;
-      navigation.navigate("Account" as never);
-      console.log("Пользователь создан:", user.uid);
-      return user;
-    } catch (error: any) {
-      console.error("Ошибка регистрации:", error.message);
-      throw error;
-    }
-  };
+export function ContinueWithGmailAndPassworsService() {
+  const { signIn } = useContinueWithGmailAndPasswors()
 
   return {
     signIn,
