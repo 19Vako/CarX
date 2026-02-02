24906a25e31ca25ad4a8821b35baafaca2725840
19Vako <152034949+19Vako@users.noreply.github.com>
2026-02-02 10:58:17 +0200

    add: Sentry.wrap()

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,28 @@
+import * as Sentry from "@sentry/react-native";
 import * as Font from "expo-font";
+import { Text } from "react-native";
 import { Provider } from "react-redux";
 import store from "../store/store";
 import RootStack from "./RootStack";
 
-export default function App() {
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  sendDefaultPii: true,
+  enableLogs: true,
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+});
+
+if (__DEV__) {
+  import("../configs/ReactotronConfig");
+}
+
+export default Sentry.wrap(function App() {
   const [fontsLoaded] = Font.useFonts({
     "SpaceMono-Regular": require("@/assets/fonts/SpaceMono-Regular.ttf"),
     "Roboto-Medium": require("@/assets/fonts/Roboto-Medium.ttf"),
@@ -14,7 +33,9 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <RootStack />
+      <Sentry.ErrorBoundary fallback={<Text>Something went wrong</Text>}>
+        <RootStack />
+      </Sentry.ErrorBoundary>
     </Provider>
   );
-}
+});
