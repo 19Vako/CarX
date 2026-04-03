3f65a7e971571798f477877e5569c5e50f48a08f
19Vako <152034949+19Vako@users.noreply.github.com>
2026-04-03 15:08:21 +0300

    feat: update location/position.ts

--- location/position.ts
+++ location/position.ts
@@ -1,33 +1,42 @@
+import {
+  setCityName,
+  setPointFrom,
+  setUserLocation,
+} from "@/store/Slices/map/mapSlice";
+import store from "@/store/store";
 import * as Location from "expo-location";
-import store from "@/store/store"; 
-import { setUserLocation, setCityName } from "@/store/Slices/map/mapSlice";
 
 export const initGlobalLocationTracking = async () => {
   try {
     const { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") return;
 
-    
     await Location.watchPositionAsync(
       {
         accuracy: Location.Accuracy.BestForNavigation,
-        timeInterval: 3000,  
+        timeInterval: 3000,
         distanceInterval: 5,
       },
+
       async (location) => {
         const { latitude, longitude } = location.coords;
         store.dispatch(setUserLocation({ latitude, longitude }));
 
         const state = store.getState();
         if (!state.location.cityName) {
-          const reverse = await Location.reverseGeocodeAsync({ latitude, longitude });
-          
+          const reverse = await Location.reverseGeocodeAsync({
+            latitude,
+            longitude,
+          });
+
           if (reverse[0]?.city) {
             store.dispatch(setCityName(reverse[0].city));
+            store.dispatch(
+              setPointFrom(reverse[0].street + " " + reverse[0].name),
+            );
           }
         }
-      }
-      
+      },
     );
   } catch (e) {
     console.error("Global tracking error:", e);
