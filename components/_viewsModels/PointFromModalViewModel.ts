9dbbedbb99bee72b1e2fd9ffd243dbdc30b6bad8
19Vako <152034949+19Vako@users.noreply.github.com>
2026-04-03 15:08:59 +0300

    feat: update: _viewsModels/PointFromModalViewModel.ts

--- components/_viewsModels/PointFromModalViewModel.ts
+++ components/_viewsModels/PointFromModalViewModel.ts
@@ -1,34 +1,31 @@
-import { useState } from "react";
-import { getCoordsFromAddress } from '@/location/geocoding';
-import { useDispatch, useSelector } from "react-redux";
-import { setManualPickupLocation } from "@/store/Slices/map/mapSlice";
-import * as MapService from '@/components/_services/MapService';
+import { getCoordsFromAddress } from "@/location/geocoding";
+import {
+  setManualPickupLocation,
+  setPointFrom,
+} from "@/store/Slices/map/mapSlice";
 import { RootState } from "@/store/store";
+import { useDispatch, useSelector } from "react-redux";
 
 export function PointFromModalViewModel() {
-  const [fromInputValue, setFromInputValue] = useState("");
-  
-  const dispatch = useDispatch(); 
-  
-  // Достаем город, который определил наш глобальный сервис position.ts
-  const cityName = useSelector((state: RootState) => state.location.cityName);
-  console.log("Текущий город из глобального состояния:", cityName);
-  
+  const dispatch = useDispatch();
+
+  const { cityName, pointFrom } = useSelector(
+    (state: RootState) => state.location,
+  );
+
   const handleSelectAddress = async () => {
-    // Используем динамический город или дефолтный, если GPS еще не сработал
     const currentCity = cityName || "Ужгород";
-    
-    const coords = await getCoordsFromAddress(fromInputValue, currentCity);
+
+    const coords = await getCoordsFromAddress(pointFrom, currentCity);
 
     if (coords) {
       dispatch(setManualPickupLocation(coords));
-      MapService.animateTo(coords);
     }
   };
 
   return {
-    fromInputValue,
-    setFromInputValue,
-    handleSelectAddress
+    pointFrom,
+    setPointFrom: (value: string) => dispatch(setPointFrom(value)),
+    handleSelectAddress,
   };
 }
