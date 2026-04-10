e5adbe1f5a1d3981572760cb0a497cb0ca5739d2
19Vako <152034949+19Vako@users.noreply.github.com>
2026-04-10 10:05:30 +0300

    refactor: improve auth flow and index screen logic
    - refactor useAuthSession to handle session state more robustly
    - update useContinueWithGmailAndPasswors for better error handling during login
    - clean up index screen logic to support new navigation structure

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,21 +1,11 @@
 import BottomWayMenu from "@/src/components/_views/BottomWayMenu";
 import DrawerButton from "@/src/components/_views/DrawerButton";
 import Map from "@/src/components/_views/Map";
-import { auth } from "@/src/configs/firebaseConfig";
-import { signOut } from "firebase/auth";
+
 import React from "react";
-import { Alert, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
-  async function handlePress() {
-    try {
-      await signOut(auth);
-    } catch (error) {
-      console.error("Ошибка при выходе:", error);
-      Alert.alert("Ошибка", "Не удалось выйти из аккаунта.");
-    }
-  }
-
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       <DrawerButton />
