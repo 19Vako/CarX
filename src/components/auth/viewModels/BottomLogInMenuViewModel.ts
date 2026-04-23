e6ef2bf6c1f8337673b5de17c7667f3b304ce452
19Vako <152034949+19Vako@users.noreply.github.com>
2026-04-23 19:59:40 +0300

    refactor(auth): isolate authentication logic into module
    - move auth services into auth domain
    - group viewModels and UI components
    - prepare for further feature-based refactor

---
+++ src/components/auth/viewModels/BottomLogInMenuViewModel.ts
@@ -0,0 +1,41 @@
+import { useKeyboardVisibility } from "@/src/hooks/useKeyboardVisibility";
+import { useEffect, useRef, useState } from "react";
+import { Animated } from "react-native";
+import { CountryCode } from "react-native-country-picker-modal";
+import { BottomLogInMenuService } from "../services/BottomLogInMenuService";
+
+export function BottomLogInMenuViewModel() {
+  const { handleContinueWithGoogle, handleSendCode, isVisible } =
+    BottomLogInMenuService();
+
+  const [countryTextCode, setCountryTextCode] = useState<CountryCode>("UA");
+  const [countryCode, setCountryCode] = useState("380");
+  const [phone, setPhone] = useState("");
+
+  const handleSendCode_ = () => {
+    handleSendCode(`+${countryCode}${phone}`);
+  };
+
+  const { keyBoardVisible } = useKeyboardVisibility();
+  const heightAnimation = useRef(new Animated.Value(300)).current;
+  useEffect(() => {
+    Animated.timing(heightAnimation, {
+      toValue: keyBoardVisible ? 555 : 400,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }, [keyBoardVisible]);
+
+  return {
+    heightAnimation,
+    handleContinueWithGoogle,
+    handleSendCode_,
+    phone,
+    setPhone,
+    isVisible,
+    countryTextCode,
+    setCountryTextCode,
+    countryCode,
+    setCountryCode,
+  };
+}
